c11d9f44d6ae2f69c3e24c581c501d09a3eb6247
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-21 22:47:35 -0300

    Use mkApp from flake-utils

--- flake.nix
+++ flake.nix
@@ -22,32 +22,28 @@
         defaultPackage = self.packages.${system}.graalvm11-ce-musl;
 
         # TODO: generate this programatically
-        apps = with self.packages.${system};
+        apps =
           let
-            mkApp = package: program:
-              {
-                type = "app";
-                program = "${package}/bin/${program}";
-              };
+            inherit (flake-utils.lib) mkApp;
           in
-          {
+          with self.packages.${system}; {
             "11/glibc/latest/native-image" = self.apps.${system}."11/glibc/21_3_0/native-image";
             "11/musl/latest/native-image" = self.apps.${system}."11/musl/21_3_0/native-image";
 
             "17/glibc/latest/native-image" = self.apps.${system}."17/glibc/21_3_0/native-image";
             "17/musl/latest/native-image" = self.apps.${system}."17/musl/21_3_0/native-image";
 
-            "11/glibc/21_2_0/native-image" = mkApp graalvm11-ce-21_2_0 "native-image";
-            "11/glibc/21_3_0/native-image" = mkApp graalvm11-ce "native-image";
+            "11/glibc/21_2_0/native-image" = mkApp { drv = graalvm11-ce-21_2_0; name = "native-image"; };
+            "11/glibc/21_3_0/native-image" = mkApp { drv = graalvm11-ce; name = "native-image"; };
 
-            "11/musl/21_2_0/native-image" = mkApp graalvm11-ce-musl-21_2_0 "native-image";
-            "11/musl/21_3_0/native-image" = mkApp graalvm11-ce-musl "native-image";
+            "11/musl/21_2_0/native-image" = mkApp { drv = graalvm11-ce-musl-21_2_0; name = "native-image"; };
+            "11/musl/21_3_0/native-image" = mkApp { drv = graalvm11-ce-musl; name = "native-image"; };
 
-            "17/glibc/21_2_0/native-image" = mkApp graalvm17-ce-21_2_0 "native-image";
-            "17/glibc/21_3_0/native-image" = mkApp graalvm17-ce "native-image";
+            "17/glibc/21_2_0/native-image" = mkApp { drv = graalvm17-ce-21_2_0; name = "native-image"; };
+            "17/glibc/21_3_0/native-image" = mkApp { drv = graalvm17-ce; name = "native-image"; };
 
-            "17/musl/21_2_0/native-image" = mkApp graalvm17-ce-musl-21_2_0 "native-image";
-            "17/musl/21_3_0/native-image" = mkApp graalvm17-ce-musl "native-image";
+            "17/musl/21_2_0/native-image" = mkApp { drv = graalvm17-ce-musl-21_2_0; name = "native-image"; };
+            "17/musl/21_3_0/native-image" = mkApp { drv = graalvm17-ce-musl; name = "native-image"; };
           };
 
         defaultApp = self.apps.${system}."11/musl/latest/native-image";
