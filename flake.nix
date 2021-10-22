d3d4c924b3ff468752b2d689e8d73b803a3ee0bd
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-21 21:29:02 -0300

    Refactor flake.nix using mkApp

--- flake.nix
+++ flake.nix
@@ -22,52 +22,33 @@
         defaultPackage = self.packages.${system}.graalvm11-ce-musl;
 
         # TODO: generate this programatically
-        apps = rec {
-          "11/glibc/latest/native-image" = self.apps.${system}."11/glibc/21_3_0/native-image";
-          "11/musl/latest/native-image" = self.apps.${system}."11/musl/21_3_0/native-image";
-          "17/glibc/latest/native-image" = self.apps.${system}."17/glibc/21_3_0/native-image";
-          "17/musl/latest/native-image" = self.apps.${system}."17/musl/21_3_0/native-image";
+        apps = with self.packages.${system};
+          let
+            mkApp = package: program:
+              {
+                type = "app";
+                program = "${package}/bin/${program}";
+              };
+          in
+          {
+            "11/glibc/latest/native-image" = self.apps.${system}."11/glibc/21_3_0/native-image";
+            "11/musl/latest/native-image" = self.apps.${system}."11/musl/21_3_0/native-image";
 
-          "11/glibc/21_2_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm11-ce-21_2_0}/bin/native-image";
-          };
+            "17/glibc/latest/native-image" = self.apps.${system}."17/glibc/21_3_0/native-image";
+            "17/musl/latest/native-image" = self.apps.${system}."17/musl/21_3_0/native-image";
 
-          "11/glibc/21_3_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm11-ce}/bin/native-image";
-          };
+            "11/glibc/21_2_0/native-image" = mkApp graalvm11-ce-21_2_0 "native-image";
+            "11/glibc/21_3_0/native-image" = mkApp graalvm11-ce "native-image";
 
-          "11/musl/21_2_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm11-ce-musl-21_2_0}/bin/native-image";
-          };
+            "11/musl/21_2_0/native-image" = mkApp graalvm11-ce-musl-21_2_0 "native-image";
+            "11/musl/21_3_0/native-image" = mkApp graalvm11-ce-musl "native-image";
 
-          "11/musl/21_3_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm11-ce-musl}/bin/native-image";
-          };
+            "17/glibc/21_2_0/native-image" = mkApp graalvm17-ce-21_2_0 "native-image";
+            "17/glibc/21_3_0/native-image" = mkApp graalvm17-ce "native-image";
 
-          "17/glibc/21_2_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm17-ce-21_2_0}/bin/native-image";
+            "17/musl/21_2_0/native-image" = mkApp graalvm17-ce-musl-21_2_0 "native-image";
+            "17/musl/21_3_0/native-image" = mkApp graalvm17-ce-musl "native-image";
           };
-
-          "17/glibc/21_3_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm17-ce}/bin/native-image";
-          };
-
-          "17/musl/21_2_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm17-ce-musl-21_2_0}/bin/native-image";
-          };
-
-          "17/musl/21_3_0/native-image" = {
-            type = "app";
-            program = "${self.packages.${system}.graalvm17-ce-musl}/bin/native-image";
-          };
-        };
 
         defaultApp = self.apps.${system}."11/musl/latest/native-image";
 
